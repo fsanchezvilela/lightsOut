08b21bb7152187d72647e837bb55c152dddb0db7
Fernando <f.sanchezvilela@gmail.com>
2019-12-25 01:08:16 +0100

    Updates

---
+++ precache-manifest.83517e6a663125093fea57fbe8ad2515.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "138fee62f53092d0b90c",
+    "url": "/lightsOut/static/css/main.fd1c2a5d.chunk.css"
+  },
+  {
+    "revision": "138fee62f53092d0b90c",
+    "url": "/lightsOut/static/js/main.138fee62.chunk.js"
+  },
+  {
+    "revision": "cd22fa7af0c3eaf7956e",
+    "url": "/lightsOut/static/js/1.cd22fa7a.chunk.js"
+  },
+  {
+    "revision": "7bb51e0662d9c55e23aa",
+    "url": "/lightsOut/static/js/runtime~main.7bb51e06.js"
+  },
+  {
+    "revision": "f3787d73ab2eafb756363eb4e8c9dc27",
+    "url": "/lightsOut/index.html"
+  }
+];
